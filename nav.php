3f294ee7c621312b8443d1f049e0472aeb72c3ef
CodeNetti <raphasimonettti@gmail.com>
2024-01-17 13:14:04 -0300

    Adicionando Include Do Nav

---
+++ nav.php
@@ -0,0 +1,48 @@
+ <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed " data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Navegação -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="inative"><a href="#" class="textmenu">Home<span class="sr-only">(current)</span></a></li>
+                    <li class="textmenu"><a href="#" class="textmenu">Games</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle textmenu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Plataformas<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">PC</a></li>
+                            <li><a href="#">Xbox</a></li>
+                            <li><a href="#">Sony</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Softwares</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Horizon Zero Dawn">
+                    </div>
+                    <button type="submit"  class="animated-button1"><a href="#" >
+                          <span></span>
+                          <span></span>
+                          <span></span>
+                          <span></span>
+                          Buscar
+                      </a>
+                    </button>
+                    <ul class="navbar-nav navbar-right login">
+                      <li><a href="#"><img src="assets/login.png"></a></li>
+                    </ul>
+                    
+                </form>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
