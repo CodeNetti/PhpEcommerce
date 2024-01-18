25047aa85fb188da5e0ef94e665404ed94342d07
CodeNetti <raphasimonettti@gmail.com>
2024-01-18 11:47:11 -0300

    Criando Include de Carrosel de Imagens

---
+++ Carrosel.php
@@ -0,0 +1,16 @@
+<div class="jumbotron text-center">
+    
+<section class="banner" id="banner">
+        <div class="slider banner-imagem banner-imagem-1 active">
+            <a style="cursor: pointer;" ></a>
+        </div>
+        <div class="slider banner-imagem banner-imagem-2">
+            <a style="cursor: pointer;"  ></a>
+        </div>
+        <div class="slider banner-imagem banner-imagem-3">
+            <a style="cursor: pointer;"  ></a>
+        </div>
+    </section>
+    
+         
+</div>
