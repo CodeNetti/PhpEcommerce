4bc30500e273e19db383c3a0c71a7de83a501edb
CodeNetti <raphasimonettti@gmail.com>
2024-01-18 11:50:03 -0300

    Aplicando Alterações, Includes e Containers

--- index.php
+++ index.php
@@ -1,19 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TellemaKeys</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-
-    
+    <!-- Inclua a biblioteca jQuery antes de outros scripts que a utilizam -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    
+    
 
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  
+    
+    
 </head>
-<body style="background-color: black;">
+<body>
  <?php include 'nav.php' ?>
+ <?php include 'Carrosel.php' ?>
+ <div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3">
+             <!-- Inclua a biblioteca jQuery antes de outros scripts que a utilizam 
+
+            <img src="assets/GAMES PC/HZD.png" class="img-responsive" style="width: 100%;">
+            <div><h2>Horizon Zero Dawn</h2></div>
+            <div><h4>R$ 60,00</h4>
+        </div>
+        <div class="col-sm-3">
+            <img src="assets/GAMES PC/HZD.png" class="img-responsive" style="width: 100%;">
+            <div><h2>Horizon Zero Dawn</h2></div>
+            <div><h4>R$ 60,00</h4>
+        </div>
+        -->
+        
+        
+
+    </div>
+ </div>
 </body>
+<script src="script.js"></script>
+
 </html>
